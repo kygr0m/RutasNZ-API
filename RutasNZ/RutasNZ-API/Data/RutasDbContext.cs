bc6d673386e4ee4bd12f2c62b5da9964560657f6
Your Name <dalcal@protonmail.com>
2024-09-08 17:58:56 +0100

    DbContext and DI added

---
+++ RutasNZ/RutasNZ-API/Data/RutasDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RutasNZ_API.Models.Domain;
+
+namespace RutasNZ_API.Data
+{
+    public class RutasDbContext : DbContext
+    {
+        public RutasDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Dificultad> Dificultades { get; set; }
+        public DbSet<Region> Regiones { get; set; }
+        public DbSet<Ruta> Rutas { get; set; }
+    }
+}
--- RutasNZ/RutasNZ-API/Program.cs
+++ RutasNZ/RutasNZ-API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using RutasNZ_API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Dependency Injection DBConnectionString
+builder.Services.AddDbContext<RutasDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionRutas")));
 
 var app = builder.Build();
 
